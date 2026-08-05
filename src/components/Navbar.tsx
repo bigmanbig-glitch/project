28811243b69118967c9ba9da32e35b8f2524183a
bigmanbig-glitch <T63752337@gmail.com>
2026-08-05 21:48:18 +0330

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -71,7 +71,7 @@ export function Navbar() {
             </span>
           </a>
 
-       <ul className="hidden items-center gap-5 lg:flex -translate-x-2">
+       <ul className="hidden items-center gap-5 lg:flex -translate-x-4">
   {LINKS.map((l) =>
     l.label === 'Collections' ? (
   <li ref={collectionsRef} key={l.href} className="relative">
